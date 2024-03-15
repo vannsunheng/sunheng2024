bc0e6efca746348c146daa3d21d8c1a07fe57182
Vann Sunheng <35210549+vannsunheng@users.noreply.github.com>
2024-03-15 15:26:04 +0700

    End of section 3

--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -9,7 +9,7 @@ namespace Infrastructure.Data
     public class StoreContext : DbContext
     {
         public StoreContext(DbContextOptions options) : base(options){
-
+            
         }
         public DbSet<Product> products{set;get;}
     }
