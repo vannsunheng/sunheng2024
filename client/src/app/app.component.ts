c270641fa7bebb41b3e40d76dd36e024f7dc7f53
Vann Sunheng <35210549+vannsunheng@users.noreply.github.com>
2024-05-11 11:49:08 +0700

    End of section 8

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Product } from './models/Product';
+import { Pagination } from './models/Pagination';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'E-Commerce';
+export class AppComponent implements OnInit {
+
+  title = 'E-commerce';
+  products: Product[]=[];
+  constructor(private http: HttpClient) {
+
+  }
+  ngOnInit(): void {
+    this.http.get<Pagination<Product[]>>("https://localhost:5001/api/products?Pagesize=50").subscribe({
+      next: response => this.products=response.data,
+      error: error => console.log(error),
+      complete: () => {
+        console.log("require complate");
+        console.log("extra statement");
+      }
+    })
+  }
 }
---
+++ client/src/app/nar-bar/nar-bar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-nar-bar',
+  templateUrl: './nar-bar.component.html',
+  styleUrls: ['./nar-bar.component.scss']
+})
+export class NarBarComponent {
+
+}
